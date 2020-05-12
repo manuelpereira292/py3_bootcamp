83a93996af2c392fd981de9fc523cb91b2c144dd
Manuel Pereira <manuelpereira292@gmail.com>
2020-05-12 17:13:49 +0100

    update

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Curso Python3 - Code Bootcamp
 
 code_pack_01 - introduction to python and jupyter notebook
 
+code_pack_02 - introduction to jupyter notebook and spyder
+
+code_pack_03 - python fundamentals 1_2
---
+++ code_pack_02/spyder_code.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+name = "Ricardo Pinto"
+a=1
+a
+
+print("hello {0} welcome to spyder!".format(name))
+
+type(a)
+
+type(name)
+help(a)
+dir(a)
+
+        
